68e97bcff288b3a594b91095e1181b46e8f70600
leo_bg_cf <leobgely@gmail.com>
2025-03-19 14:23:49 +0100

    switch currency & crypto from list to enums

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,31 +1,35 @@
-const List<String> currenciesList = [
-  'AUD',
-  'BRL',
-  'CAD',
-  'CNY',
-  'EUR',
-  'GBP',
-  'HKD',
-  'IDR',
-  'ILS',
-  'INR',
-  'JPY',
-  'MXN',
-  'NOK',
-  'NZD',
-  'PLN',
-  'RON',
-  'RUB',
-  'SEK',
-  'SGD',
-  'USD',
-  'ZAR'
-];
+enum Currencie {
+  AUD,
+  BRL,
+  CAD,
+  CNY,
+  EUR,
+  GBP,
+  HKD,
+  IDR,
+  ILS,
+  INR,
+  JPY,
+  MXN,
+  NOK,
+  NZD,
+  PLN,
+  RON,
+  RUB,
+  SEK,
+  SGD,
+  USD,
+  ZAR
+}
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
+enum Crypto {
+  BTC,
+  ETH,
+  LTC,
+}
+
+List<String> get currenciesList => Currencie.values.map((value) => value.name).toList();
+
+List<String> get cryptoList => Crypto.values.map((value) => value.name).toList();
 
 class CoinData {}
