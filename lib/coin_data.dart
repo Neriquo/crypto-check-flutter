693e88c9965ab9b534f0d7f8cf7d26cd4359797f
Armen0 <hugodev04@gmail.com>
2025-03-19 16:12:40 +0100

    finished project

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -26,6 +29,45 @@ const List<String> cryptoList = [
   'BTC',
   'ETH',
   'LTC',
+  'XRP',
+  'SOL',
+  'DOGE',
+  'SHIB',
 ];
 
-class CoinData {}
+// la liste des images des crypto
+const List<String> imageUrl = [
+  "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=040",
+  "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=040",
+  "https://cryptologos.cc/logos/litecoin-ltc-logo.png?v=040",
+  "https://cryptologos.cc/logos/xrp-xrp-logo.png?v=040",
+  "https://cryptologos.cc/logos/solana-sol-logo.png?v=040",
+  "https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=040",
+  "https://cryptologos.cc/logos/shiba-inu-shib-logo.png?v=040"
+];
+
+// on envoit une requete API pour chaque crypto avec la devise choisie
+class CoinData {
+  Future<Map<String, String>> getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    
+    for (String crypto in cryptoList) {
+      String url = 'https://rest.coinapi.io/v1/exchangerate/$crypto/$selectedCurrency';
+      http.Response response = await http.get(
+        Uri.parse(url),
+        headers: {'X-CoinAPI-Key': 'cdba6bb6-a239-4459-808b-104023561927'}, // Replace with your API key
+      );
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(5);
+      } else {
+        print(response.statusCode);
+        throw Exception('Failed to load coin data');
+      }
+    }
+    
+    return cryptoPrices;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,18 +10,34 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String? selectedCurrency = 'USD';
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
+
+  // on envoit une requete API pour chaque crypto avec la devise choisie
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await CoinData().getCoinData(selectedCurrency!);
+      isWaiting = false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+      isWaiting = false;
+    }
+  }
 
   CupertinoPicker iOSPicker() {
     List<Text> pickerItems = [];
 
     for (String currency in currenciesList) {
       var newItem = Text(currency);
-
       pickerItems.add(newItem);
     }
 
     return CupertinoPicker(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.black,
       itemExtent: 32,
       onSelectedItemChanged: (selectedIndex) {
         print(selectedIndex);
@@ -39,7 +54,6 @@ class _PriceScreenState extends State<PriceScreen> {
         child: Text(currency),
         value: currency,
       );
-
       dropdownItems.add(newItem);
     }
 
@@ -49,6 +63,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData();
         });
       },
     );
@@ -58,40 +73,67 @@ class _PriceScreenState extends State<PriceScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bitcoin check'),
+        title: Text('💲Crypto Check💲'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          Column(
+            children: [
+              // Pour chaque crypto, on affiche une bulle avec le logo, la crypto et sa veleur
+              for (String crypto in cryptoList)
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                  child: Card(
+                    color: Colors.white70,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ListTile(
+                      leading: Image(
+                        image: NetworkImage(imageUrl[cryptoList.indexOf(crypto)]),
+                        fit: BoxFit.cover,
+                        width: 35.0,
+                        height: 35.0,
+                      ),
+                      title: Text(
+                        // lors d'un appel API, on affiche un texte en attendant la réponse de l'API
+                        isWaiting
+                            ? '1 $crypto = Loading...'
+                            : '1 $crypto = ${coinValues[crypto]} $selectedCurrency',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Change the currency here.",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
-          ),
+              Container(
+                height: 100.0,
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(bottom: 30.0),
+                color: Colors.grey.shade900,
+                child:
+                    // si on est sur iOS, on affiche un picker, sinon on affiche un dropdown
+                Platform.isIOS ? iOSPicker() : androidDropdown(),
+              ),
+            ],
+          )
         ],
       ),
     );
