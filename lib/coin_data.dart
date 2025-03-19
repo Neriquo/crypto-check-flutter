2bc2ece7aa920a550623a4472f33a47a062a4003
hnaceur <hella.naceur@gmail.com>
2025-03-19 16:24:18 +0100

    fonctionne mais faut revoir

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,18 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  String apiKey = 'a02c661f-97f4-4f76-8e7c-ac3b7a1a8d9e';
+  String apiURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+  // taux de change depuis l'API
+  Future<String> getCoinData(String crypto, String currency) async {
+    String requestURL = '$apiURL/$crypto/$currency?apikey=$apiKey';
+
+    http.Response response = await http.get(Uri.parse(requestURL));
+    var decodedData = jsonDecode(response.body);
+
+    double rate = decodedData['rate'];
+    return rate.toStringAsFixed(2); // retourne le taux avec 2 décimales
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
-import 'package:bitcoin_ticker/coin_data.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'coin_data.dart';
+
+const apiKey = 'a02c661f-97f4-4f76-8e7c-ac3b7a1a8d9e'; //CL2 API
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,37 +13,40 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String? selectedCurrency = 'USD';
+  String selectedCurrency = 'USD';
 
-  CupertinoPicker iOSPicker() {
-    List<Text> pickerItems = [];
+  Map<String, String> coinValues = {};
 
-    for (String currency in currenciesList) {
-      var newItem = Text(currency);
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
-      pickerItems.add(newItem);
+  void getData() async {
+    for (String crypto in cryptoList) {
+      var response = await http.get(
+        Uri.parse('$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey'),
+      );
+
+      var decodedData = jsonDecode(response.body);
+
+      setState(() {
+        double rate = decodedData['rate'];
+        coinValues[crypto] = rate.toStringAsFixed(2);
+      });
     }
-
-    return CupertinoPicker(
-      backgroundColor: Colors.lightBlue,
-      itemExtent: 32,
-      onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
-      },
-      children: pickerItems,
-    );
   }
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
-
     for (String currency in currenciesList) {
-      var newItem = DropdownMenuItem(
-        child: Text(currency),
-        value: currency,
+      dropdownItems.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
       );
-
-      dropdownItems.add(newItem);
     }
 
     return DropdownButton<String>(
@@ -47,7 +54,8 @@ class _PriceScreenState extends State<PriceScreen> {
       items: dropdownItems,
       onChanged: (value) {
         setState(() {
-          selectedCurrency = value;
+          selectedCurrency = value!;
+          getData();
         });
       },
     );
@@ -55,6 +63,17 @@ class _PriceScreenState extends State<PriceScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> cryptoCards = [];
+    for (String crypto in cryptoList) {
+      cryptoCards.add(
+        CryptoCard(
+          cryptoCurrency: crypto,
+          currencyValue: coinValues[crypto] ?? '?',
+          selectedCurrency: selectedCurrency,
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Bitcoin check'),
@@ -63,35 +82,54 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: cryptoCards,
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            child: androidDropdown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  CryptoCard(
+      {required this.currencyValue,
+      required this.selectedCurrency,
+      required this.cryptoCurrency});
+
+  final String currencyValue;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 18.0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $currencyValue $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
