a7a00a5cd8d73b7c531a73bd20546526049b3049
leo_bg_cf <leobgely@gmail.com>
2025-03-19 15:32:22 +0100

    change displayed currency depending of selected currency

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -69,7 +69,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ? ${selectedCurrency?.name ?? "no currency selected"}',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
